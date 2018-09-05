a88f964d98f31b59236af2119a50fa6489362935
ArpanMangal <mangalarpan@gmail.com>
2018-09-05 18:41:08 +0530

    Class Structure

--- Game.h
+++ Game.h
@@ -1,10 +1,59 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <vector>
+#include "Move.h"
+using namespace std;
+
 class Game
 {
   private:
+    // Game parameters
+    int numRings;
+    int boardSize;
+    int numRingsToRemove;
+    int numRingsForRow;
+
+    // Game Board
+    int **board;
+    vector<pair<int, int>> ringsPositive; // Positions of rings of the positive player
+    vector<pair<int, int>> ringsNegative; // Positions of rings of the positive player
+
+    // Game State
+    int playerToMove; // -1 or 1
+    int gameState;    // 1: Place Ring | 2: Select Ring and Move Ring | 3: Remove a Row | 4: Remove a Ring
+
   public:
+    /**
+     * @constructor
+     */
+    Game();
+
+    /**
+     * Return all the moves that the `playerToMove` can play from the given state 
+     */
+    vector<Move> getAllMoves();
+
+    /**
+     * Play the move, and return success status
+     */
+    bool makeMove(Move move);
+
+    /** 
+     * Unplay the move, and return success status
+     */
+    bool unmakeMove(Move move);
+
+    /**
+     * Returns whether the current game state is a win for the `playerToMove`
+     */
+    bool isTerminalState();
+
+    /**
+     * Returns the utility of this state w.r.t. `playerToMove`
+     * Called each time when minimax is cut-off
+     */
+    double getUtility(); // @Soumya
 };
 
 #endif // GAME_H
--- Player.h
+++ Player.h
@@ -1,10 +1,43 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#define bestAction pair<double, vector<Move>>
+
+#include "Game.h"
+#include "Move.h"
+#include <vector>
+using namespace std;
+
 class Player
 {
   private:
+    /** 
+     * The Game state maintained by our player
+     */
+    Game game;
+
+    /**
+     * Returns the best Action to perform from the current game state
+     */
+    bestAction max_value(int depth);
+
+    /**
+     * Takes in input for the opponent's move
+     * Processes the move and play's the move in our game state
+     */
+    void playOpponentMove();
+
   public:
+    /**
+     * @Constructor
+     * Input: Player 0 (1) or Player 1 (-1)
+     */
+    Player(int playerType);
+
+    /**
+     * Game playing function
+     */
+    void playGame();
 };
 
 #endif // PLAYER_H
