91e7000293dbaaf67e6a079d892b092ea974108c
ArpanMangal <mangalarpan@gmail.com>
2018-09-05 23:50:10 +0530

    Functions for converting between polar and array coordinates

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
- all: main.cpp Player.cpp Game.cpp Move.cpp
-	  g++ --std=c++11 -o main main.cpp Player.cpp Game.cpp Move.cpp
+ all: main.cpp Player.cpp Game.cpp Move.cpp Utility.cpp 
+	  g++ --std=c++11 -o main main.cpp Player.cpp Game.cpp Move.cpp Utility.cpp 
 
 clean: 
 	rm -f main
--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+#include "Utility.h"
 #include "Player.h"
 #include <iostream>
+#include <assert.h>
 using namespace std;
+
+bool testerUtility ();
 
 int main(int argc, char **argv)
 {
+    testerUtility();
+    return 0;
+
+
     int player;
     int N;
     double timeLeft;
@@ -17,4 +25,27 @@ int main(int argc, char **argv)
     yinsh->playGame();
 
     return 0;
+}
+
+bool testerUtility () {
+    Utility *tester = new Utility();
+
+    // Tests the conversion functions
+    int boardSize = 101;
+    for (int i = 0; i < boardSize; i++) {
+        for (int j = 0; j < boardSize; j++) {
+            pair<int, int> arrCoord = make_pair(i, j);
+            pair<int, int> polarCoord = tester->arrayToPolar(arrCoord, boardSize);
+            // cout << "P | " << polarCoord.first << " " << polarCoord.second << endl;
+            arrCoord = tester->polarToArray(polarCoord, boardSize);
+
+            // if (arrCoord.first != i || arrCoord.second != j) {
+                // cout << i << " " << j << " | " << polarCoord.first << " " << polarCoord.second << " | " << arrCoord.first << " " << arrCoord.second <<  endl;
+            // }
+            assert(arrCoord.first == i);
+            assert(arrCoord.second == j);
+        }
+    }
+
+    return true;
 }
