be5a675e1b0f8ac0bc974d81ba236bec2472d5cb
ArpanMangal <mangalarpan@gmail.com>
2018-09-07 10:38:06 +0530

    -7 for disallowed positions

--- Game.cpp
+++ Game.cpp
@@ -1,7 +1,9 @@
 #include "Game.h"
+#include "Utility.h"
 using namespace std;
 
-Game::Game(int numberOfRings) {
+Game::Game(int numberOfRings)
+{
     // Initialise board specifications
     numRings = numberOfRings;
     boardSize = numberOfRings * 2 + 1;
@@ -9,52 +11,72 @@ Game::Game(int numberOfRings) {
     numRingsForRow = numberOfRings;
 
     // Allocate a board
-    board = new int* [boardSize];
-    for (int i = 0; i < boardSize; i++) {
+    board = new int *[boardSize];
+    for (int i = 0; i < boardSize; i++)
+    {
         board[i] = new int[boardSize];
     }
 
     // Initialize the board
-    // The disallowed board positions are also alloted 0 for now
-    for (int i = 0; i < boardSize; i++) {
-        for (int j = 0; j < boardSize; j++) {
+    // Allowed: Empty (0) | Disallowed: -7
+    Utility *allowed = new Utility();
+    for (int i = 0; i < boardSize; i++)
+    {
+        for (int j = 0; j < boardSize; j++)
+        {
             board[i][j] = 0;
+
+            pair<int, int> polar = allowed->arrayToPolar(make_pair(i, j), boardSize);
+            int hex = polar.first, pos = polar.second;
+            if (hex > boardSize / 2 || (hex == boardSize / 2 && pos % hex == 0))
+            {
+                // not allowed
+                board[i][j] = -7;
+            }
         }
     }
 
     // Initialise the game state
     playerToMove = 1; // Always player 0 moves first
-    gameState = 1; // Place Ring
+    gameState = 1;    // Place Ring
 }
 
-int Game::getPlayerToMove () {
+int Game::getPlayerToMove()
+{
     return playerToMove;
 }
 
-int Game::getGameState() {
+int Game::getGameState()
+{
     return gameState;
 }
 
-bool Game::makeMove (Move move) {
+bool Game::makeMove(Move move)
+{
     return false;
 }
 
-bool Game::unmakeMove (Move move) {
+bool Game::unmakeMove(Move move)
+{
     return false;
 }
 
-vector<Move> Game::getAllMoves() {
+vector<Move> Game::getAllMoves()
+{
     return vector<Move>();
 }
 
-bool Game::isTerminalState() {
-    if (gameState == 1) {
+bool Game::isTerminalState()
+{
+    if (gameState == 1)
+    {
         // Game in ring placing stage
         return false;
     }
 
-    if (numRings - ringsPositive.size() >= numRingsToRemove || 
-    numRings - ringsNegative.size() >= numRingsToRemove) {
+    if (numRings - ringsPositive.size() >= numRingsToRemove ||
+        numRings - ringsNegative.size() >= numRingsToRemove)
+    {
         // Either one of the player has removed sufficient number of rings
         return true;
     }
@@ -63,7 +85,8 @@ bool Game::isTerminalState() {
     return false;
 }
 
-double Game::getUtility () {
+double Game::getUtility()
+{
     // Return the utility of the current state
     // Utility of win state should be highest and of lose state should be minimum
     // @Soumya
